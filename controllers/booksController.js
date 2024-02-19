301aaa2878c22a28e44ddea140fc72c6e7c25214
salma <salmasherif856@gmail.com>
2024-02-18 22:09:23 -0800

    user books logic

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -1,17 +1,17 @@
-const path = require('path');
-const parentDir = path.resolve(__dirname, '..');
-const booksModelPath = path.join(parentDir, 'models', 'BooksModel');
+const path = require("path");
+const parentDir = path.resolve(__dirname, "..");
+const booksModelPath = path.join(parentDir, "models", "BooksModel");
 const Book = require(booksModelPath);
 const asyncWrapper = require("./../lib/asyncWrapper");
-const CustomError = require("./../errors/customError")
+const CustomError = require("./../errors/customError");
 
-
+//------------ adding new book ---------------
 const addBook = async (req, res, next) => {
-  const newBook = new Book(req.body)
-  const [err, book] = await asyncWrapper(newBook.save())
+  const newBook = new Book(req.body);
+  const [err, book] = await asyncWrapper(newBook.save());
 
   if (err) {
-    return next(new CustomError("Error adding the book!", 500))
+    return next(new CustomError("Error adding the book!", 500));
   }
 
   res.status(201).json({
@@ -20,9 +20,9 @@ const addBook = async (req, res, next) => {
       book,
     },
   });
-
 };
 
+// -------------- editing a book -------------
 const editBook = async (req, res) => {
   req.body.updatedAt = new Date();
   const [err, updatedBook] = await asyncWrapper(
@@ -32,7 +32,7 @@ const editBook = async (req, res) => {
   );
 
   if (err) {
-    return next(new CustomError("Error updating the book!", 500))
+    return next(new CustomError("Error updating the book!", 500));
   }
 
   res.status(200).json({
@@ -43,35 +43,37 @@ const editBook = async (req, res) => {
   });
 };
 
+// -------------- delete a book ----------------
 const deleteBook = async (req, res) => {
-  const [err] = await asyncWrapper(Book.findByIdAndDelete(req.params.id))
+  const [err] = await asyncWrapper(Book.findByIdAndDelete(req.params.id));
 
   if (err) {
-    return next(new CustomError("Error deleting the book!", 500))
+    return next(new CustomError("Error deleting the book!", 500));
   }
   res.sendStatus(204);
 };
 
-// applying pagination
+//------------ get all books or based on the category --------------
 const getBooks = async (req, res) => {
-  const page = req.query.page * 1 || 1
-  const limit = req.query.limit * 1 || 20
-  const skip = (page - 1) * limit
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 20;
+  const skip = (page - 1) * limit;
   const category = req.query.category;
 
-  if(req.query.page){
-    const booksCount = await Book.countDocuments()
-    if(skip >= booksCount) return next(new CustomError("No more books to view!", 404))
+  if (req.query.page) {
+    const booksCount = await Book.countDocuments();
+    if (skip >= booksCount)
+      return next(new CustomError("No more books to view!", 404));
   }
 
   const [err, books] = await asyncWrapper(
     category
       ? Book.find({ category: category }).skip(skip).limit(limit)
       : Book.find().skip(skip).limit(limit)
-  )
+  );
 
   if (err) {
-    return next(new CustomError("Error getting the books!", 500))
+    return next(new CustomError("Error getting the books!", 500));
   }
   res.status(200).json({
     status: "success",
@@ -81,6 +83,7 @@ const getBooks = async (req, res) => {
   });
 };
 
+// --------------- get book by id -----------------
 const getBookById = async (req, res, next) => {
   const id = req.params.id * 1;
 
@@ -89,8 +92,8 @@ const getBookById = async (req, res, next) => {
     .populate({ path: "category", select: "---" })
     .populate({ path: "auhtor", select: "----" })
     .exec();
-  if(err){
-    return next(new CustomError("Error getting the book!", 500))
+  if (err) {
+    return next(new CustomError("Error getting the book!", 500));
   }
   res.status(200).json({
     status: "success",
@@ -100,15 +103,15 @@ const getBookById = async (req, res, next) => {
   });
 };
 
-// find books for specific user
-// add books for specific user
-// edit books for specific user
-// delete books for specifis user
+
+// body checking : not specified properties
+// most popular books
+// search for book
 
 module.exports = {
   addBook,
   editBook,
   deleteBook,
   getBooks,
-  getBookById
+  getBookById,
 };
---
+++ controllers/userBooksController.js
@@ -0,0 +1,114 @@
+const UserBook = require("./../models/UserBooksModel");
+
+
+// ---------get user books by user id according to the status------------
+const getUserBooksByStatus = async (req, res, next) => {
+  const { userId, status } = req.query;
+  const page = req.query.page * 1 || 1;
+  const limit = req.query.limit * 1 || 20;
+  const skip = (page - 1) * limit;
+
+  if (req.query.page) {
+    const userBooksCount = await UserBook.countDocuments({
+      user: userId,
+      status,
+    });
+    if (skip >= userBooksCount)
+      return next(new CustomError("No more user books to view!", 404));
+  }
+
+  const [err, userBooks] = await asyncWrapper(
+    UserBook.find({ user: userId, status })
+      .populate("Book")
+      .skip(skip)
+      .limit(limit)
+  );
+
+  if (err) {
+    return next(new CustomError("Error getting user books!", 500));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      userBooks,
+    },
+  });
+};
+
+//--------- update user book review or rating or status ------------
+const editUserBook = async (req, res, next) => {
+  const { userBookId } = req.params;
+  const { review, rating, status } = req.body;
+
+  const updates = {};
+  if (review) updates.review = review;
+  if (rating) updates.rating = rating;
+  if (status) updates.status = status;
+
+  updates.updated_at = new Date();
+
+  const [err, updatedUserBook] = await asyncWrapper(
+    UserBook.findByIdAndUpdate(userBookId, updates, { new: true })
+  );
+
+  if (err) {
+    return next(new CustomError("Error updating user book!", 500));
+  }
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      updatedUserBook,
+    },
+  });
+};
+
+// ------------ add new book to the users books ----------------
+const addUserBook = async (req, res, next) => {
+  const { userId, bookId, status } = req.body;
+
+  const newUserBook = new UserBook({
+    user: userId,
+    book: bookId,
+    status: status,
+  });
+
+  const [err, addedUserBook] = await asyncWrapper(newUserBook.save());
+
+  if (err) {
+    return next(new CustomError("Error adding user book!", 500));
+  }
+
+  res.status(201).json({
+    status: "success",
+    data: {
+      addedUserBook,
+    },
+  });
+};
+
+//------------ delete user book by user id ----------------
+const deleteUserBook = async (req, res, next) => {
+  const { userBookId } = req.params;
+
+  const [err, deletedUserBook] = await asyncWrapper(
+    UserBook.findByIdAndDelete(userBookId)
+  );
+
+  if (err) {
+    return next(new CustomError("Error deleting user book!", 500));
+  }
+
+  res.status(200).json({
+    status: "success",
+    message: "User book deleted successfully",
+  });
+};
+
+module.exports = {
+  getUserBooksByStatus,
+  editUserBook,
+  addUserBook,
+  deleteUserBook,
+};
