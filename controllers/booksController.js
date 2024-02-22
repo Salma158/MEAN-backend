656afce54e7664ecb5620b0ffb4cad03de021490
salma <salmasherif856@gmail.com>
2024-02-22 17:08:50 +0200

    fix issue

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -3,7 +3,7 @@ const parentDir = path.resolve(__dirname, "..");
 const booksModelPath = path.join(parentDir, "models", "BooksModel");
 const Book = require(booksModelPath);
 const userbooksModelPath = path.join(parentDir, "models", "UserBooksModel");
-const UserBook = requie(userbooksModelPath)
+const UserBook = require(userbooksModelPath)
 const asyncWrapper = require("../lib/asyncWrapper");
 const CustomError = require("./../lib/customError");
 
