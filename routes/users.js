8e6a8c7909860c6cb0c4e63e54967c71f867dc65
yossrmohammed <yossr.khatab5@gmail.com>
2024-02-22 10:21:03 +0200

    update user router

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userController = require('../controllers/users');
 const auth = require('../middlewares/auth');
+const multerMiddleware = require('../middlewares/uploadImg');
 
-router.post('/', userController.signUp);
+
+router.post('/', multerMiddleware, userController.addOne);
 router.post('/login', userController.logIn);
 router.get('/', auth.authorization, auth.restrictTo('admin'), userController.getAll);
 router.patch('/:id', auth.authorization, auth.restrictTo('admin'), userController.updateRole)
