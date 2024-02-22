d550a424df53302597b49eb81f36c058d595e9d6
yossrmohammed <yossr.khatab5@gmail.com>
2024-02-22 10:22:32 +0200

    add middleware to upload photos

---
+++ middlewares/uploadImg.js
@@ -0,0 +1,40 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: (req, res, cb) => {
+        cb(null, 'profile/')
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname)); // Generate unique filename
+    }
+
+});
+const checkType = (file, cb) => {
+    const types = /jpeg|jpg|png|gif/;
+    const extension = types.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = types.test(file.mimetype);
+
+    if (mimetype && extension) {
+        return cb(null, true);
+    } else {
+        cb('Error: Images only!');
+    }
+}
+const upload = multer({
+    storage: storage,
+    fileFilter: function (req, file, cb) {
+        checkType(file, cb);
+    }
+}).single('photo');
+
+const multerMiddleware = (req, res, next) => {
+    upload(req, res, (err) => {
+        if (err) {
+            return res.status(400).json({ message: 'File upload failed', error: err });
+        }
+        next(); // Move on to the next middleware or route handler
+    });
+};
+
+module.exports = multerMiddleware;
